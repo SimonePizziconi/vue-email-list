2cc1eb3ad1879f7793458ee0af3d386b9bdfcf7b
SimonePizziconi <simonepizziconi05@gmail.com>
2024-06-27 17:36:43 +0200

    feat: add methods to display emails in page

--- js/main.js
+++ js/main.js
@@ -6,18 +6,19 @@ const { createApp } = Vue;
   createApp({
     data() {
       return {
-        email:``
+        emailList: [
+            
+        ]
       }
     },
     methods:{
-
+        createEmails(){
+            for(let i = 0; i < 10; i++) {
+                axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`).then(item => {
+                    this.emailList.push(item.data.response);
+                    console.log(this.emailList);
+                });
+            }
+        }
     },
-    mounted(){
-        axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`).then(item => {
-            const result = item.data.response;
-            this.email = result;
-            
-        });
-    }
-
-  }).mount('#app')
+    }).mount('#app')
